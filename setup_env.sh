255c936997d0c00fb4059acc03363e589e7afcb2
Zhuokai Zhao <zhuokai@uchicago.edu>
2023-01-25 20:00:56 -0600

    Tested example.

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+# check if NVidia GPU is available
+if [[ $(lshw -C display | grep vendor) =~ "NVIDIA Corporation" ]]; then
+    echo "Installing PyTorch with CUDA"
+    conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
+else
+    echo "Installing CPU-only PyTorch"
+    conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
+fi
+
+# other dependencies
+pip install -U deepctr-torch
+conda install -c conda-forge pandas -y
