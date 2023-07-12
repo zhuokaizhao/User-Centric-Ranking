d9f08887c5d791ccbee7756fafb3b93c3e488545
Zhuokai Zhao <zhuokai@uchicago.edu>
2023-07-12 08:35:39 -0400

    Small update.

--- setup_env.sh
+++ setup_env.sh
@@ -1,11 +1,11 @@
 # check if NVidia GPU is available
-if [[ $(lshw -C display | grep vendor) =~ "NVIDIA Corporation" ]]; then
-    echo "Installing PyTorch with CUDA"
-    conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-else
-    echo "Installing CPU-only PyTorch"
-    conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
-fi
+# if [[ $(lshw -C display | grep vendor) =~ "NVIDIA Corporation" ]]; then
+echo "Installing PyTorch with CUDA"
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
+# else
+#     echo "Installing CPU-only PyTorch"
+#     conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
+# fi
 
 # other dependencies
 pip install -U deepctr-torch
